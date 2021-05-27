b2813984fffec1b90d9b04c1f58dbf9c6fdaa47d
Marcelo dos Santos <mdssjc@gmail.com>
2021-05-26 21:31:08 -0300

    feat: add first time

---
+++ testing-spring-boot/section-3-test-driven-development-by-example/tdd-by-example/src/main/java/guru/springframework/Dollar.java
@@ -0,0 +1,17 @@
+package guru.springframework;
+
+/**
+ * @author Marcelo dos Santos
+ */
+public class Dollar {
+
+    public int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    public void times(int multiplier) {
+        amount *= multiplier;
+    }
+}
