dd111b3b6752f021dd3dca29beda3fb09430a19b
Marcelo dos Santos <mdssjc@gmail.com>
2021-06-05 14:05:53 -0300

    refactor: the root of all evil

--- testing-spring-boot/section-3-test-driven-development-by-example/tdd-by-example/src/test/java/guru/springframework/MoneyTest.java
+++ testing-spring-boot/section-3-test-driven-development-by-example/tdd-by-example/src/test/java/guru/springframework/MoneyTest.java
@@ -11,28 +11,22 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 public class MoneyTest {
 
     @Test
-    void testMultiplicationDollar() {
+    void testMultiplication() {
         Money five = Money.dollar(5);
         assertEquals(Money.dollar(10), five.times(2));
         assertEquals(Money.dollar(15), five.times(3));
+
+        Money fiveFranc = Money.franc(5);
+        assertEquals(Money.franc(10), fiveFranc.times(2));
+        assertEquals(Money.franc(15), fiveFranc.times(3));
     }
 
     @Test
-    void testEqualityDollar() {
+    void testEquality() {
         assertEquals(Money.dollar(5), Money.dollar(5));
         assertNotEquals(Money.dollar(5), Money.dollar(8));
         assertNotEquals(Money.dollar(5), Money.franc(5));
-    }
 
-    @Test
-    void testMultiplicationFranc() {
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
-    }
-
-    @Test
-    void testEqualityFranc() {
         assertEquals(Money.franc(5), Money.franc(5));
         assertNotEquals(Money.franc(5), Money.franc(8));
     }
