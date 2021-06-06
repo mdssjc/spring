a3a853fe888cabd94f7de14307db835f8aa263e2
Marcelo dos Santos <mdssjc@gmail.com>
2021-06-05 22:02:14 -0300

    feat: add lc beer service - data model

---
+++ spring-boot-microservices-with-spring-cloud/mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,8 @@
+package guru.springframework.msscbeerservice.web.model;
+
+/**
+ * @author Marcelo dos Santos
+ */
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
