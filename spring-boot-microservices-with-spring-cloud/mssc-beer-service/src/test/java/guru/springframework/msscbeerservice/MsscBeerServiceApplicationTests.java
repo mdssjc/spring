d2ce1d98e205ccec7f9e00c845df4d3a040ab928
Marcelo dos Santos <mdssjc@gmail.com>
2021-06-05 21:29:33 -0300

    chore: add lc beer service - initial spring boot project creation

---
+++ spring-boot-microservices-with-spring-cloud/mssc-beer-service/src/test/java/guru/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package guru.springframework.msscbeerservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Marcelo dos Santos
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MsscBeerServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
