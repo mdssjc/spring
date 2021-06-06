8bdbcb9899ff35dd1dff0d521ecf411549398503
Marcelo dos Santos <mdssjc@gmail.com>
2021-06-05 23:01:01 -0300

    test: lc beer service - junit 5 controller tests

--- spring-boot-microservices-with-spring-cloud/mssc-beer-service/src/test/java/guru/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
+++ spring-boot-microservices-with-spring-cloud/mssc-beer-service/src/test/java/guru/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -1,14 +1,11 @@
 package guru.springframework.msscbeerservice;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * @author Marcelo dos Santos
  */
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class MsscBeerServiceApplicationTests {
 
