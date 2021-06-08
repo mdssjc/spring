6b0afb49712db016eb1745c08166ca2f477232b4
Marcelo dos Santos <mdssjc@gmail.com>
2021-06-07 22:21:09 -0300

    test: junit 5 test instance lifecycle demo

--- testing-spring-boot/section-4-getting-started-with-junit-5/intro-junit5/src/test/java/guru/springframework/GreetingTest.java
+++ testing-spring-boot/section-4-getting-started-with-junit-5/intro-junit5/src/test/java/guru/springframework/GreetingTest.java
@@ -1,7 +1,6 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 /**
  * @author Marcelo dos Santos
@@ -9,6 +8,11 @@ import org.junit.jupiter.api.Test;
 class GreetingTest {
 
     private Greeting greeting;
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before - I'm only called once!!!");
+    }
 
     @BeforeEach
     void setUp() {
@@ -25,5 +29,15 @@ class GreetingTest {
     @Test
     void testHelloWorld() {
         System.out.println(greeting.helloWorld("Marcelo"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("In after each...");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After - I'm only called once!!!");
     }
 }
