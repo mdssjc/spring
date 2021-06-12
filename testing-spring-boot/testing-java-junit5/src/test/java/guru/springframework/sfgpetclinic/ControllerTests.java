ffd2bac0fc27ff484315f13572de18d01323b9db
Marcelo dos Santos <mdssjc@gmail.com>
2021-06-12 18:29:27 -0300

    test: using junit default test methods

--- testing-spring-boot/testing-java-junit5/src/test/java/guru/springframework/sfgpetclinic/ControllerTests.java
+++ testing-spring-boot/testing-java-junit5/src/test/java/guru/springframework/sfgpetclinic/ControllerTests.java
@@ -1,11 +1,18 @@
 package guru.springframework.sfgpetclinic;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
 
 /**
  * @author Marcelo dos Santos
  */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Tag("controllers")
 public interface ControllerTests {
 
+    @BeforeAll
+    default void beforeAll() {
+        System.out.println("Lets do something here");
+    }
 }
