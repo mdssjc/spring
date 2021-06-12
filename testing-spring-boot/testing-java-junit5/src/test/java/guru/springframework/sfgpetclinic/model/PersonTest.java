663ee5d7eeab3484374b474e00cd9f0e17a0179b
Marcelo dos Santos <mdssjc@gmail.com>
2021-06-12 18:37:56 -0300

    test: junit test dependency injection

--- testing-spring-boot/testing-java-junit5/src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ testing-spring-boot/testing-java-junit5/src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -1,9 +1,7 @@
 package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.sfgpetclinic.ModelTests;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -39,5 +37,10 @@ class PersonTest implements ModelTests {
     @DisplayName("My Repeated Test")
     void myRepeatedTest() {
 
+    }
+
+    @RepeatedTest(5)
+    void myRepeatedTestWithDI(TestInfo testInfo, RepetitionInfo repetitionInfo) {
+        System.out.println(testInfo.getDisplayName() + ": " + repetitionInfo.getCurrentRepetition());
     }
 }
