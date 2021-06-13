75f0383c6cfe3a072ae6aca81c8997a11d6ae4bd
Marcelo dos Santos <mdssjc@gmail.com>
2021-06-13 19:09:39 -0300

    test: creating mockito mocks inline

---
+++ testing-spring-boot/testing-java-junit5/src/test/java/guru/springframework/InlineMockTest.java
@@ -0,0 +1,21 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
+/**
+ * @author Marcelo dos Santos
+ */
+public class InlineMockTest {
+
+    @Test
+    void testInlineMock() {
+        Map mapMock = mock(Map.class);
+
+        assertEquals(mapMock.size(), 0);
+    }
+}
