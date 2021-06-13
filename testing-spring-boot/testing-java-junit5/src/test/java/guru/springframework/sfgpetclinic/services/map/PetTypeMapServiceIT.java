7955209270e0387fc47cdd3a814cfe8faf0fe329
Marcelo dos Santos <mdssjc@gmail.com>
2021-06-12 21:56:02 -0300

    test: unit tests vs integration tests

---
+++ testing-spring-boot/testing-java-junit5/src/test/java/guru/springframework/sfgpetclinic/services/map/PetTypeMapServiceIT.java
@@ -0,0 +1,34 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Marcelo dos Santos
+ */
+class PetTypeMapServiceIT {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
