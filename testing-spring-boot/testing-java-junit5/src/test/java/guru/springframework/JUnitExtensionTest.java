d7b8b1780ef4e7c1383924b6d4c6cee6b92351a7
Marcelo dos Santos <mdssjc@gmail.com>
2021-06-13 19:15:35 -0300

    test: junit mockito extension

---
+++ testing-spring-boot/testing-java-junit5/src/test/java/guru/springframework/JUnitExtensionTest.java
@@ -0,0 +1,23 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Map;
+
+/**
+ * @author Marcelo dos Santos
+ */
+@ExtendWith(MockitoExtension.class)
+class JUnitExtensionTest {
+
+    @Mock
+    Map<String, Object> mapMock;
+
+    @Test
+    void testMock() {
+        mapMock.put("keyValue", "foo");
+    }
+}
