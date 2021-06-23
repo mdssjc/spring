7e28fb1765dcc2facb481dae5e5dde3b7c427560
Marcelo dos Santos <mdssjc@gmail.com>
2021-06-22 22:44:55 -0300

    test: junit 5 laurel test

---
+++ testing-spring-boot/tb2g-testing-spring/src/test/java/org/springframework/samples/petclinic/sfg/junit5/LaurelHearingInterpreterTest.java
@@ -0,0 +1,27 @@
+package org.springframework.samples.petclinic.sfg.junit5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.sfg.BaseConfig;
+import org.springframework.samples.petclinic.sfg.HearingInterpreter;
+import org.springframework.samples.petclinic.sfg.LaurelConfig;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Marcelo dos Santos
+ */
+@SpringJUnitConfig(classes = {BaseConfig.class, LaurelConfig.class})
+class LaurelHearingInterpreterTest {
+
+    @Autowired
+    HearingInterpreter hearingInterpreter;
+
+    @Test
+    void whatIHeard() {
+        String word = hearingInterpreter.whatIHeard();
+
+        assertEquals("Laurel", word);
+    }
+}
