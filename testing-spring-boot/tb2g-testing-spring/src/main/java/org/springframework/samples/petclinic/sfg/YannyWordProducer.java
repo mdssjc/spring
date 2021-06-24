f8a872346776ae46de8f73427dfbe0304b19aa76
Marcelo dos Santos <mdssjc@gmail.com>
2021-06-23 22:39:16 -0300

    feat: add setting active profiles for tests

--- testing-spring-boot/tb2g-testing-spring/src/main/java/org/springframework/samples/petclinic/sfg/YannyWordProducer.java
+++ testing-spring-boot/tb2g-testing-spring/src/main/java/org/springframework/samples/petclinic/sfg/YannyWordProducer.java
@@ -1,10 +1,14 @@
 package org.springframework.samples.petclinic.sfg;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 /**
  * @author Marcelo dos Santos
  */
+@Profile("yanny")
+@Primary
 @Component
 public class YannyWordProducer implements WordProducer {
 
