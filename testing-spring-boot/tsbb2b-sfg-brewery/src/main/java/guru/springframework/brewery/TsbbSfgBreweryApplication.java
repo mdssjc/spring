971a84975026354ff670b8dfef6f4f8f58a4eb4d
Marcelo dos Santos <mdssjc@gmail.com>
2021-06-28 22:29:23 -0300

    chore: add updating spring boot 2.x to junit 5

--- testing-spring-boot/tsbb2b-sfg-brewery/src/main/java/guru/springframework/brewery/TsbbSfgBreweryApplication.java
+++ testing-spring-boot/tsbb2b-sfg-brewery/src/main/java/guru/springframework/brewery/TsbbSfgBreweryApplication.java
@@ -3,11 +3,13 @@ package guru.springframework.brewery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Marcelo dos Santos
+ */
 @SpringBootApplication
 public class TsbbSfgBreweryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TsbbSfgBreweryApplication.class, args);
     }
-
 }
--- testing-spring-boot/tsbb2b-sfg-brewery/src/test/java/guru/springframework/brewery/TsbbSfgBreweryApplicationTests.java
+++ testing-spring-boot/tsbb2b-sfg-brewery/src/test/java/guru/springframework/brewery/TsbbSfgBreweryApplicationTests.java
@@ -1,16 +1,15 @@
 package guru.springframework.brewery;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+/**
+ * @author Marcelo dos Santos
+ */
 @SpringBootTest
 public class TsbbSfgBreweryApplicationTests {
 
     @Test
     public void contextLoads() {
     }
-
 }
