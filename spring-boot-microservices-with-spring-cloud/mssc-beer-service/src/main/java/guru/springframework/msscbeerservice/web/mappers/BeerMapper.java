ac4391abf2ca0e4424d00c3d19a861bf0ba8087d
Marcelo dos Santos <mdssjc@gmail.com>
2021-09-22 22:43:58 -0300

    feat: add lc - implementing mapstruct

---
+++ spring-boot-microservices-with-spring-cloud/mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/web/mappers/BeerMapper.java
@@ -0,0 +1,16 @@
+package guru.springframework.msscbeerservice.web.mappers;
+
+import guru.springframework.msscbeerservice.domain.Beer;
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+/**
+ * @author Marcelo dos Santos
+ */
+@Mapper(uses = DateMapper.class)
+public interface BeerMapper {
+
+    BeerDto beerToBeerDto(Beer beer);
+
+    Beer beerDtoToBeer(BeerDto dto);
+}
