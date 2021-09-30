f78a1950d042a96903ef22eaad2b572d2a48a158
Marcelo dos Santos <mdssjc@gmail.com>
2021-09-29 22:25:09 -0300

    chore: add json testing with spring boot

---
+++ spring-boot-microservices-with-spring-cloud/mssc-jackson-examples/src/main/java/guru/springframework/msscjacksonexamples/MsscJacksonExamplesApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.msscjacksonexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscJacksonExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscJacksonExamplesApplication.class, args);
+    }
+}
---
+++ spring-boot-microservices-with-spring-cloud/mssc-jackson-examples/src/test/java/guru/springframework/msscjacksonexamples/MsscJacksonExamplesApplicationTests.java
@@ -0,0 +1,12 @@
+package guru.springframework.msscjacksonexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MsscJacksonExamplesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
