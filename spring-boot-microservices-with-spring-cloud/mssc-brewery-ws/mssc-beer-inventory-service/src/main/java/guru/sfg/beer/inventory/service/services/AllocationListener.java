a7f95dd48d3b9813e4e4e8527a21c8dfa2c00a76
Marcelo dos Santos <mdssjc@gmail.com>
2021-11-10 22:56:37 -0300

    feat: add assignment 10 - assignment - complete allocation message flow

---
+++ spring-boot-microservices-with-spring-cloud/mssc-brewery-ws/mssc-beer-inventory-service/src/main/java/guru/sfg/beer/inventory/service/services/AllocationListener.java
@@ -0,0 +1,43 @@
+package guru.sfg.beer.inventory.service.services;
+
+import guru.sfg.beer.inventory.service.config.JmsConfig;
+import guru.sfg.brewery.model.events.AllocateOrderRequest;
+import guru.sfg.brewery.model.events.AllocateOrderResult;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Marcelo dos Santos
+ */
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class AllocationListener {
+
+    private final AllocationService service;
+    private final JmsTemplate template;
+
+    @JmsListener(destination = JmsConfig.ALLOCATE_ORDER_QUEUE)
+    public void listen(AllocateOrderRequest request) {
+        AllocateOrderResult.AllocateOrderResultBuilder builder = AllocateOrderResult.builder();
+        builder.beerOrderDto(request.getBeerOrderDto());
+
+        try {
+            boolean allocationResult = service.allocateOrder(request.getBeerOrderDto());
+
+            if (allocationResult) {
+                builder.pendingInventory(false);
+            } else {
+                builder.pendingInventory(true);
+            }
+        } catch (Exception ex) {
+            log.error("Allocation failed for Order id: " + request.getBeerOrderDto().getId());
+            builder.allocationError(true);
+        }
+
+        template.convertAndSend(JmsConfig.ALLOCATE_ORDER_RESPONSE_QUEUE, builder.build());
+    }
+}
---
+++ spring-boot-microservices-with-spring-cloud/mssc-brewery-ws/mssc-beer-inventory-service/src/main/java/guru/sfg/brewery/model/events/AllocateOrderResult.java
@@ -0,0 +1,21 @@
+package guru.sfg.brewery.model.events;
+
+import guru.sfg.brewery.model.BeerOrderDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Marcelo dos Santos
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AllocateOrderResult {
+
+    private BeerOrderDto beerOrderDto;
+    private Boolean allocationError = false;
+    private Boolean pendingInventory = false;
+}
