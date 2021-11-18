6bd4316944e619b37d0e795a642925b6f9e95eb8
Marcelo dos Santos <mdssjc@gmail.com>
2021-11-17 22:01:11 -0300

    test: add create integration test for new order to allocate

---
+++ spring-boot-microservices-with-spring-cloud/mssc-brewery-ws/mssc-beer-order-service/src/test/java/guru/sfg/beer/order/service/BeerOrderManagerImplIT.java
@@ -0,0 +1,75 @@
+package guru.sfg.beer.order.service;
+
+import guru.sfg.beer.order.service.domain.BeerOrder;
+import guru.sfg.beer.order.service.domain.BeerOrderLine;
+import guru.sfg.beer.order.service.domain.BeerOrderStatusEnum;
+import guru.sfg.beer.order.service.domain.Customer;
+import guru.sfg.beer.order.service.repositories.BeerOrderRepository;
+import guru.sfg.beer.order.service.repositories.CustomerRepository;
+import guru.sfg.beer.order.service.services.BeerOrderManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * @author Marcelo dos Santos
+ */
+@SpringBootTest
+class BeerOrderManagerImplIT {
+
+    @Autowired
+    BeerOrderManager manager;
+
+    @Autowired
+    BeerOrderRepository repository;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    Customer customer;
+
+    UUID beerId = UUID.randomUUID();
+
+    @BeforeEach
+    void setUp() {
+        customer = customerRepository.save(Customer.builder()
+                .customerName("Test Customer")
+                .build());
+    }
+
+    @Test
+    void testNewToAllocated() {
+        BeerOrder beerOrder = createBeerOrder();
+
+        BeerOrder savedBeerOrder = manager.newBeerOrder(beerOrder);
+
+        assertNotNull(savedBeerOrder);
+        assertEquals(BeerOrderStatusEnum.ALLOCATED, savedBeerOrder.getOrderStatus());
+    }
+
+    public BeerOrder createBeerOrder() {
+        BeerOrder beerOrder = BeerOrder.builder()
+                .customer(customer)
+                .build();
+
+        Set<BeerOrderLine> lines = new HashSet<>();
+        lines.add(BeerOrderLine.builder()
+                .beerId(beerId)
+                .upc("12345")
+                .orderQuantity(1)
+                .beerOrder(beerOrder)
+                .build());
+
+        beerOrder.setBeerOrderLines(lines);
+
+        return beerOrder;
+    }
+}
