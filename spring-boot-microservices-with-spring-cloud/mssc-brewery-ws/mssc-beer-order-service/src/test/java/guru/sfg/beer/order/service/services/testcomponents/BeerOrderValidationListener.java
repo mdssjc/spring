4137214d98745398f789128ca66fcb88dd97f8d6
Marcelo dos Santos <mdssjc@gmail.com>
2021-11-22 22:55:51 -0300

    test: add testing failed validation

--- spring-boot-microservices-with-spring-cloud/mssc-brewery-ws/mssc-beer-order-service/src/test/java/guru/sfg/beer/order/service/services/testcomponents/BeerOrderValidationListener.java
+++ spring-boot-microservices-with-spring-cloud/mssc-brewery-ws/mssc-beer-order-service/src/test/java/guru/sfg/beer/order/service/services/testcomponents/BeerOrderValidationListener.java
@@ -22,11 +22,17 @@ public class BeerOrderValidationListener {
 
     @JmsListener(destination = JmsConfig.VALIDATE_ORDER_QUEUE)
     public void list(Message msg) {
+        boolean isValid = true;
+
         ValidateOrderRequest request = (ValidateOrderRequest) msg.getPayload();
+
+        if (request.getBeerOrder().getCustomerRef() != null && request.getBeerOrder().getCustomerRef().equals("fail-validation")) {
+            isValid = false;
+        }
 
         template.convertAndSend(JmsConfig.VALIDATE_ORDER_RESPONSE_QUEUE,
                 ValidateOrderResult.builder()
-                        .isValid(true)
+                        .isValid(isValid)
                         .orderId(request.getBeerOrder().getId())
                         .build());
     }
