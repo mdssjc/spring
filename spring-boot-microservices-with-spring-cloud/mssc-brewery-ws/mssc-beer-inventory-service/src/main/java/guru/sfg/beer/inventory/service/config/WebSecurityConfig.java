7b862bcc705ccac66a9c0ce5bfa2abae881e0cfe
Marcelo dos Santos <mdssjc@gmail.com>
2021-12-02 23:17:45 -0300

    feat: add secure inventory service with spring security

---
+++ spring-boot-microservices-with-spring-cloud/mssc-brewery-ws/mssc-beer-inventory-service/src/main/java/guru/sfg/beer/inventory/service/config/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package guru.sfg.beer.inventory.service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author Marcelo dos Santos
+ */
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}
